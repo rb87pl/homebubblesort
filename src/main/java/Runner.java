23300e726cd3fbecced5e19efc661580592ab424
Robert B <rb87pl@gmail.com>
2018-09-19 22:48:17 +0200

    poprawione 1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -10,9 +10,9 @@ public class Runner {
             swap=0;
             for (int i = 0; i < table.length-1 ; i++) {
                 if (table[i] > table[i + 1]) {
-                    temp = table[i + 1];
+                    temp = table[i];
                     table[i] = table[i+1];
-                    table[i] = temp;
+                    table[i + 1] = temp;
                     swap++;
                 }
             }
